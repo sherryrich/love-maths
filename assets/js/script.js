370364424596f54656c0118a998e22177eb58441
sherryrich <ricsherry@gmail.com>
2022-03-24 17:26:20 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
-console.log("CONNECTED");
+function runGame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+
+function incrementScore() {
+    
+}
+
+
+function incrementWrongAnswer() {
+    
+}
+
+
+function displayAdditionQuestion() {
+    
+}
+
+
+
+function displaySubtractQuestion() {
+    
+}
+
+
+function displayMultiplyQuestion() {
+    
+}
+
