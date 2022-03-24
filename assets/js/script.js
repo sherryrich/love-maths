99fff2f3cfa03fd66824dc2b05165eabac68457d
sherryrich <ricsherry@gmail.com>
2022-03-24 18:18:42 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("you clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
@@ -22,11 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * 
  */
 
-function runGame() {
+function runGame(gameType) {
         // creates two random numbers between 1 annd 25
         let num1 = Math.floor(Math.random() * 25 + 1);
         let num2 = Math.floor(Math.random() * 25 + 1);
-}
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Uknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}. Aborting!`;
+    }
+
+    }
+
 
 function checkAnswer() {
     
@@ -47,8 +57,10 @@ function incrementWrongAnswer() {
 }
 
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 
@@ -61,4 +73,3 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
     
 }
-
