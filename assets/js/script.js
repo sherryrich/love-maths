d44a37dd595da8726203643b60703c8356cf6e97
sherryrich <ricsherry@gmail.com>
2022-03-25 12:57:49 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer(); 
+             }
+    })
+    
     runGame("addition");
 })
 
@@ -25,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+document.getElementById("answer-box").value = "";
+document.getElementById("answer-box").focus();
+
         // creates two random numbers between 1 annd 25
         let num1 = Math.floor(Math.random() * 25 + 1);
         let num2 = Math.floor(Math.random() * 25 + 1);
@@ -34,8 +43,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
-    }
-    else { 
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    } else { 
         alert(`Uknown game type: ${gameType}`);
         throw `Uknown game type: ${gameType}. Aborting!`;
     }
@@ -79,7 +89,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } 
+    } else if (operator === "/") { 
+        return [operand1 / operand2, "division"];
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -126,4 +138,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+   
+    
 }
